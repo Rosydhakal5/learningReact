b2db2165a27d65b889c964450b433150afef2f19
Rosy <rosydhakal333@gmail.com>
2023-12-19 14:55:06 +1100

    learning to add expressions in react

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <>
-  <h1> Netflix list </h1>
-  <p> Here are my favourite 5 series:</p>
-  <ol>
-    <li> I </li>
-    <li> Mine </li>
-    <li> You </li>
-    <li> We </li>
-    <li> our </li>
-  </ol>
-</>
+// ReactDOM.render(
+//   <>
+//   <h1> Netflix list </h1>
+//   <p> Here are my favourite 5 series:</p>
+//   <ol>
+//     <li> I </li>
+//     <li> Mine </li>
+//     <li> You </li>
+//     <li> We </li>
+//     <li> our </li>
+//   </ol>
+// </>
   
 
-  , document.getElementById("root")
+//   , document.getElementById("root")
+
+// );
+
+
+//============Javascript Expression in JSX in ReactJs=========
+
+const fName = "Rosy Dhakal";
+ReactDOM.render(
+  <>
+  <h1> Hi my name is {fName} </h1>
+   <p> I am Super happy to learn React</p>
+ 
+   </>
+,document.getElementById("root")
 
 );
-
